5123a0b6019fb871b2f8655759a71abcd576ff37
Patrick Morkert <patrick@morkert.com>
2020-12-23 15:58:14 -0500

    hi!

--- Recipe Searcher/ContentView.swift
+++ Recipe Searcher/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Search Edamam for recipes")
+                Spacer()
+            }
+            .padding(.leading, 10.0)
+            TextField("Enter a food", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal, 10.0)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Search")
+                    .padding(.top)
+            }
+            Spacer()
+        }
+
     }
 }
 
