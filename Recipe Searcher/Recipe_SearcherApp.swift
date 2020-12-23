d7d9599c938c1323f381f089592cf178a7ad0c14
Patrick Morkert <patrick@morkert.com>
2020-12-23 15:30:23 -0500

    Initial Commit

---
+++ Recipe Searcher/Recipe_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_SearcherApp.swift
+//  Recipe Searcher
+//
+//  Created by Patrick Morkert on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
